939a3ab6a158e114f896c0bce4622d5921d58474
jhonjairo916 <jhonjairo.916@hotmail.com>
2021-11-20 13:46:25 -0500

    Envio de correo, falta la configuracion de los metodos para sengrid

--- src/services/autenticacion.service.ts
+++ src/services/autenticacion.service.ts
@@ -1,10 +1,24 @@
 import {injectable, /* inject, */ BindingScope} from '@loopback/core';
-
+import { repository } from '@loopback/repository';
+import { PersonaRepository } from '../repositories';
+const generador = require("password-generator");
+const cryptoJs = require("crypto-js");
 @injectable({scope: BindingScope.TRANSIENT})
 export class AutenticacionService {
-  constructor(/* Add @inject to inject parameters */) {}
+  constructor(
+    @repository(PersonaRepository)
+    public personaRepository:PersonaRepository
+  ) {}
 
   /*
    * Add service methods here
    */
+  GenerarClave(){
+    let clave = generador(8,false);
+    return clave;
+  }
+  CifrarClave(clave:string){
+    let claveCifrada = cryptoJs.MD5(clave).toString();
+    return claveCifrada;
+  }
 }
