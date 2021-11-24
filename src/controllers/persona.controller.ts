e28e430bd7badcfcd98e08f811b46e87c1673820
jhonjairo916 <jhonjairo.916@hotmail.com>
2021-11-24 12:42:53 -0500

    Se crean las credenciales para ingreso

--- src/controllers/persona.controller.ts
+++ src/controllers/persona.controller.ts
@@ -1,35 +1,55 @@
-import { service } from '@loopback/core';
+import {service} from '@loopback/core';
 import {
   Count,
   CountSchema,
   Filter,
   FilterExcludingWhere,
   repository,
-  Where,
+  Where
 } from '@loopback/repository';
 import {
-  post,
-  param,
-  get,
-  getModelSchemaRef,
-  patch,
-  put,
-  del,
-  requestBody,
-  response,
+  del, get,
+  getModelSchemaRef, HttpErrors, param, patch, post, put, requestBody,
+  response
 } from '@loopback/rest';
-import {Persona} from '../models';
+import {Credenciales, Persona} from '../models';
 import {PersonaRepository} from '../repositories';
-import { AutenticacionService } from '../services';
+import {AutenticacionService} from '../services';
 const fetch = require("node-fetch");
 
 export class PersonaController {
   constructor(
     @repository(PersonaRepository)
-    public personaRepository : PersonaRepository,
+    public personaRepository: PersonaRepository,
     @service(AutenticacionService)
     public servicioAutenticacion: AutenticacionService
-  ) {}
+  ) { }
+
+  @post('/identificarPersona', {
+    responses: {
+      '200': {
+        description: "Identificacion de usuarios"
+      }
+    }
+  })
+  async identificarPersona(
+    @requestBody() credenciales: Credenciales
+  ) {
+    let p = await this.servicioAutenticacion.IdentificarPersona(credenciales.usuario, credenciales.clave);
+    if (p) {
+      let token = this.servicioAutenticacion.GenerarTokenJWT(p);
+      return {
+        datos: {
+          nombre: p.nombres,
+          correo: p.correo,
+          id: p.id
+        },
+        tk: token
+      }
+    } else {
+      throw new HttpErrors[401]("Datos invalidos");
+    }
+  }
 
   @post('/personas')
   @response(200, {
@@ -58,10 +78,10 @@ export class PersonaController {
     let destino = persona.correo;
     let asunto = "registro en la plataforma";
     let contenido = `Hola ${persona.nombres}, su nombre de usuario es: ${persona.correo} y su contraseña es: ${clave}`;
-    /*fetch(`http://127.0.0.1:5000/envio-correo?correo_destino=${destino}&asunto= ${asunto}&contenido=${contenido}`)
-    .then((data:any)=>{
-      console.log(data);
-    })*/
+    /*fetch(`${Llaves.urlServicionNotificaciones}/envio-correo?correo_destino=${destino}&asunto= ${asunto}&contenido=${contenido}`)
+      .then((data: any) => {
+        console.log(data);
+      })*/
     return p;
   }
 
--- src/services/autenticacion.service.ts
+++ src/services/autenticacion.service.ts
@@ -1,24 +1,58 @@
-import {injectable, /* inject, */ BindingScope} from '@loopback/core';
-import { repository } from '@loopback/repository';
-import { PersonaRepository } from '../repositories';
+import { /* inject, */ BindingScope, injectable} from '@loopback/core';
+import {repository} from '@loopback/repository';
+import {Llaves} from '../config/llaves';
+import {Persona} from '../models';
+import {PersonaRepository} from '../repositories';
 const generador = require("password-generator");
 const cryptoJs = require("crypto-js");
+const jwt = require("jsonwebtoken");
 @injectable({scope: BindingScope.TRANSIENT})
 export class AutenticacionService {
   constructor(
     @repository(PersonaRepository)
-    public personaRepository:PersonaRepository
-  ) {}
+    public personaRepository: PersonaRepository
+  ) { }
 
   /*
    * Add service methods here
    */
-  GenerarClave(){
-    let clave = generador(8,false);
+  GenerarClave() {
+    let clave = generador(8, false);
     return clave;
   }
-  CifrarClave(clave:string){
+  CifrarClave(clave: string) {
     let claveCifrada = cryptoJs.MD5(clave).toString();
     return claveCifrada;
+  }
+  IdentificarPersona(usuario: string, clave: string) {
+    try {
+      let p = this.personaRepository.findOne({where: {correo: usuario, clave: clave}});
+      if (p) {
+        return p;
+      }
+      return false;
+    } catch {
+      return false;
+    }
+  }
+  GenerarTokenJWT(Persona: Persona) {
+    let token = jwt.sign({
+      data: {
+        id: Persona.id,
+        correo: Persona.correo,
+        nombre: Persona.nombres + " " + Persona.apellidos
+      }
+    },
+      Llaves.claveJWT);
+    return token;
+  }
+  ValidarToketJWT(token: string) {
+    try {
+      let datos = jwt.verify(token, Llaves.claveJWT);
+      return datos;
+    } catch {
+      return false;
+
+    }
   }
 }
